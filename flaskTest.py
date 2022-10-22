ae48719af69985f4f106155a1e0f373b087393da
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2022-10-22 21:15:22 +0530

    fWorking Flask

--- flaskTest.py
+++ flaskTest.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 
 @app.route("/")
 def hello_world():
     return render_template('userHomePage.html')
-
-
-@app.route("/harry")
-def hello_world1():
-    return "<p>Hello, Harry!</p>"
 
 
 app.run(debug=True)
