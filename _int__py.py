b3fc488cbaa327c4a47e2e9099a4147f08da71e4
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2022-11-06 15:32:17 +0530

    06 Nov 2022

--- _int__py.py
+++ _int__py.py
@@ -1,11 +1,17 @@
+import requests
+import json
 from flask import Flask, render_template
+
 
 app = Flask(__name__, template_folder='template')
 
 
-@app.route("/")
-def hello_world():
-    return render_template('userHomePage.html')
+@app.route("/", methods=['GET'])
+def index():
+    req = requests.get('https://api.unsplash.com/search/photos?query=affirmation&client_id=6yk_In27AcY3hxuqRfm_p4kpv1tZHm109oUy_UJE22g')
+    data = json.loads(req.content)
+
+    return render_template('userHomePage.html', data=data['results'])
 
 
 app.run(debug=True)
