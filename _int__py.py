ac8789ea64f7919a595f7642b9239ac5f95d0e30
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2022-11-06 16:22:31 +0530

    Back to NoAPI

--- _int__py.py
+++ _int__py.py
@@ -1,17 +1,13 @@
-import requests
-import json
 from flask import Flask, render_template
 
 
 app = Flask(__name__, template_folder='template')
 
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def index():
-    req = requests.get('https://api.unsplash.com/search/photos?query=affirmation&client_id=6yk_In27AcY3hxuqRfm_p4kpv1tZHm109oUy_UJE22g')
-    data = json.loads(req.content)
-
-    return render_template('userHomePage.html', data=data['results'])
+    
+    return render_template('userHomePage.html')
 
 
 app.run(debug=True)
