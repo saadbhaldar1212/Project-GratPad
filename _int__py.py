6771371112a22ac5a920bd2ce4deac64853fc3d5
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2022-12-04 17:58:55 +0530

    add def contact()

--- _int__py.py
+++ _int__py.py
@@ -5,9 +5,11 @@ app = Flask(__name__, template_folder='template')
 
 
 @app.route("/")
-def index():
-    
+def index():    
     return render_template('userHomePage.html')
+
+def contact():
+    return render_template('userContactPage.html')
 
 
 app.run(debug=True)
