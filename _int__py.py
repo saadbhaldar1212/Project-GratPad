4d1a8f78ff8dc4b07505878ede54efd249abc53d
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2022-12-10 02:28:46 +0530

    embed text editor
    //TODO:
    - TEMPLATE INHERITANCE
    - CONTACT FORM (ALREADY DOWNLOADED)
    - COMPLETE UI FOR JOURNALING

--- _int__py.py
+++ _int__py.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+'''from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-
+'''
 app = Flask(__name__, template_folder='template')
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'projectbysaadandaakanksha'
+'''db = SQLAlchemy(app)
+app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite///database.db'
+app.config['SECRET_KEY'] = '1234'
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(80), nullable=False)
-
-import os
-
+'''
 @app.route("/")
 def index():    
     return render_template('userHomePage.html')
@@ -31,5 +29,4 @@ def userSignUp():
     return render_template('userSignUp.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
