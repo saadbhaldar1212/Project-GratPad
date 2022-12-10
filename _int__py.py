535613b1e1d74acf4d8f7a5fdfb6b6490554d1be
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2022-12-11 03:39:26 +0530

    ..

--- _int__py.py
+++ _int__py.py
@@ -1,42 +1,48 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 
+app = Flask(__name__)
 
-app = Flask(__name__, template_folder='template')
-
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/db_name'
 app.config['SECRET_KEY'] = '1234'
 db = SQLAlchemy()
-'''
-class User(db.Model, UserMixin):
+
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(80), nullable=False)
-'''
+
+
 @app.route("/")
-def index():    
+def index():
     return render_template('userHomePage.html')
+
 
 @app.route("/welcomePage")
 def mainPage():
     return render_template('userMainPage.html')
 
+
 @app.route("/contactus")
 def contact():
     return render_template('userContactPage.html')
+
 
 @app.route("/about")
 def about():
     return 'About us'
 
+
 @app.route("/userSignIn")
 def userSignIn():
     return render_template('userSignIn.html')
 
+
 @app.route("/userSignUp")
 def userSignUp():
     return render_template('userSignUp.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
