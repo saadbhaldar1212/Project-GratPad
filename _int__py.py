cb87237668693dfae0da71f187ffc5421b88c723
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2022-12-12 01:07:15 +0530

    play with db

--- _int__py.py
+++ _int__py.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from datetime import datetime
 
 app = Flask(__name__, template_folder='template')
 
@@ -9,10 +9,49 @@ app.config['SECRET_KEY'] = '1234'
 db = SQLAlchemy()
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+class User(db.Model):
+    u_id = db.Column(db.Integer, primary_key=True)
+    u_uname = db.Column(db.String(100), nullable=False)
+    u_email = db.Column(db.String(100), nullable=False)
+    u_pass = db.Column(db.String(100), nullable=False)
+    u_repass = db.Column(db.String(100), nullable=False)
+
+
+class Login(db.Model, User):
+    l_id = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self):
+        super().__init__()
+
+
+class Contact(db.Model):
+    c_id = db.Column(db.Integer, primary_key=True)
+    c_fname = db.Column(db.String(100), nullable=False)
+    c_lname = db.Column(db.String(100), nullable=False)
+    c_email = db.Column(db.String(100), nullable=False)
+    c_msg = db.Column(db.String(200), nullable=False)
+
+
+class Journal(db.Model):
+    j_id = db.Column(db.Integer, primary_key=True)
+    j_date = db.Column(db.String(50), nullable=True)
+    j_time = db.Column(db.String(50), nullable=True)
+    j_content = db.Column(db.String(5000), nullable=False)
+
+
+class Product(db.Model):
+    p_id = db.Column(db.Integer, primary_key=True)
+    p_name = db.Column(db.String(100), nullable=False)
+    p_desc = db.Column(db.String(100), nullable=False)
+    p_price = db.Column(db.Float, nullable=False)
+    p_quant = db.Column(db.Integer, nullable=False)
+    p_img = db.Column(db.String(100), nullable=False)
+
+
+class Admin(db.Model):
+    a_id = db.Column(db.Integer, primary_key=True)
+    a_uname = db.Column(db.String(100), nullable=False)
+    a_pass = db.Column(db.String(100), nullable=False)
 
 
 @app.route("/")
